f1ea1a434b2dc569cc618a3b1e119300e023988f
Ruban <Ruban@example.com>
2025-11-24 20:19:27 +0100

    better

--- ex00/srcs/megaphone.cpp
+++ ex00/srcs/megaphone.cpp
@@ -1,5 +1,6 @@
-//#include <cctype>
+#include <cctype>
 #include <iostream>
+#include <string>
 
 //1) check if only no args to print the right thing
 //2) if there is args convert each char with toupper
@@ -7,22 +8,20 @@
 
 int main(int argc, char* argv[]) 
 {
-   int i = 1;
-   int j = 0;
-   char ch;
-
    if(argc == 1)
-      std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-   while(argv[i])
    {
-      while(argv[i][j])
+       std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+       return(0);
+   }
+   for(int i = 1; i < argc; ++i)
+   {
+      std::string s = argv[i];
+      for (std::size_t j = 0; j < s.size(); ++j)
       {
-         ch = toupper((argv[i][j]));
-         std::cout << ch;
-         j++;
+            unsigned char uc = static_cast<unsigned char>(s[j]);
+            s[j] = static_cast<char>(std::toupper(uc));
       }
-      j = 0;
-      i++;
+      std::cout << s;
    }
    std::cout << std::endl;
    return 0;
