d07f6e7ed8c622ebdafaa7cb1f5c16678ceb6875
Ruban <Ruban@example.com>
2025-11-25 16:04:18 +0100

    commentaries for ex00

--- ex00/srcs/megaphone.cpp
+++ ex00/srcs/megaphone.cpp
@@ -2,9 +2,15 @@
 #include <iostream>
 #include <string>
 
-//1) check if only no args to print the right thing
-//2) if there is args convert each char with toupper
-//3) end with a endl (\n)
+//1) check if no args to print the right msg + endline then return(0)
+//2) for loop to iterate all parameters
+//3) We create a string that will hold the value (to make it easier and safer to modify)
+//4) for loop until we finished to iterate inside of the string 
+//5) inside the loop we need to cast into an unsigned char for the following reason:
+//   if we send a char that is negative it may create an undefined behavior (like the char 'é')
+//6) we also need to use a char bcs of how toupper works.
+//7) print the converted string and get back at the start of the loop until there is no args lefts
+//8) finish with a endl.
 
 int main(int argc, char* argv[]) 
 {
