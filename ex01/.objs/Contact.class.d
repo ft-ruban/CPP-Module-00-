55b28b3d0e8d209a5cab12f3488628aed3b3f10b
Lea Devoudelle-Royer <ldevoude@z1r9p5.42lyon.fr>
2025-12-07 11:59:35 +0100

    close to release

--- ex01/.objs/Contact.class.d
+++
@@ -1,4 +0,0 @@
-.objs/Contact.class.o: srcs/Contact.class.cpp \
-  srcs/../includes/Contact.class.hpp
-
-srcs/../includes/Contact.class.hpp:
--- ex01/.objs/PhoneBook.class.d
+++
@@ -1,7 +0,0 @@
-.objs/PhoneBook.class.o: srcs/PhoneBook.class.cpp \
-  srcs/../includes/PhoneBook.class.hpp \
-  srcs/../includes/Contact.class.hpp
-
-srcs/../includes/PhoneBook.class.hpp:
-
-srcs/../includes/Contact.class.hpp:
